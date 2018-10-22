7b573db3b24684b6ff45f26f4fd9c09614fe8640
yg271334757 <271334757@qq.com>
2018-10-22 15:42:33 +0100

    binary search

---
+++ 33搜索旋转排序数列.py
@@ -0,0 +1,29 @@
+class Solution:
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        if not nums:
+            return -1
+        l = 0
+        r = len(nums) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[l] <= nums[mid]:
+                if nums[l]<= target<= nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            else:
+                if nums[mid]<= target <=nums[r]:
+                    l = mid + 1
+                else:
+                    r = mid - 1 
+        return -1
+        # if target in nums:
+        #   return nums.index(target)
+        #return -1
