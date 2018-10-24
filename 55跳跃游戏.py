d07ec286ee0015f83c49c1afd4bbac300256ac30
yg271334757 <271334757@qq.com>
2018-10-24 15:33:40 +0100

    shit

---
+++ 55跳跃游戏.py
@@ -0,0 +1,12 @@
+class Solution:
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        max_reach = 0
+        for i, v in enumerate(nums):
+            if i > max_reach:
+                return False
+            max_reach = max(max_reach, i+v)
+        return True
