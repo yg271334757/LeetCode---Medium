f5cff3c03db0428e6f9f34283869067800fda84c
yg271334757 <271334757@qq.com>
2018-11-05 15:30:11 +0000

    bainary search

---
+++ 153寻找旋转排序数组中的最小值.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # return min(nums)
+        l = 0
+        r = len(nums) - 1
+        while l != r:
+            mid = (l + r) // 2
+            if nums[mid] > nums[r]:
+                l = mid + 1
+            else:
+                r = mid
+        return nums[l]
